23bf0e159b49f3b7b363626f7432b82ef0fb1ea3
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-24 22:30:19 +0530

    [Mihir] Add. Queue created

--- StacksAndQueueDSProblem/Program.cs
+++ StacksAndQueueDSProblem/Program.cs
@@ -17,7 +17,16 @@ namespace StacksAndQueueDSProblem
             stack.Pop();
             stack.IsEmpty();
             stack.Display();
+
+            Console.WriteLine("\n========================================================================");
+            Console.WriteLine("Queue problem");
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
             Console.ReadKey();
+
         }
     }
 }
